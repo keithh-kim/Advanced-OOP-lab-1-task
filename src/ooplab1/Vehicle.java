3d27e01417c033bc8a58d451e32c724fa1f9deeb
keithh-kim <keithh.kimani@gmail.com>
2026-04-17 13:09:10 +0300

    Adding attributes to vehicle class

--- src/ooplab1/Vehicle.java
+++ src/ooplab1/Vehicle.java
@@ -2,6 +2,9 @@ package ooplab1;
 
 public abstract class Vehicle {
     //Atrributes
+    protected String brand;
+    protected String fuelType;
+    protected int Speed;
 
     //Abstract methods
     protected abstract void accelerate();
