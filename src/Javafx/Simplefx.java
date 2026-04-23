6c6daa96f14e7ae4a5e2fa605faed2c3a7014efd
keithh-kim <keithh.kimani@gmail.com>
2026-04-23 13:03:31 +0300

    Using java fx

---
+++ src/Javafx/Simplefx.java
@@ -0,0 +1,59 @@
+package Javafx;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
+
+public class Simplefx extends Application {
+    // A simple dialog with a button and a textbox
+    Text msg = new Text("Welcome to OOP 2");
+    Text Name = new Text("Name: ");
+    TextField txtname = new TextField();
+    Button btnOk = new Button("OK");
+    GridPane gridPane = new GridPane();
+
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // Gridlayout configs
+        // import javafx.geometry.Pos;
+        // import java.fx.geometry.Insets;
+        gridPane.setMinSize(400, 200);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setHgap(10);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        //Adding components to the layout
+        gridPane.add(msg, 1, 1);
+        gridPane.add(Name, 0, 1);
+        gridPane.add(txtname, 1, 1);
+        gridPane.add(btnOk, 0, 2);
+
+        // Creating the scene
+        // import javafx.scene.Scene
+        Scene scene = new Scene(gridPane);
+
+        // Configure Scene
+        stage.setTitle("OOP II E");
+        stage.setScene(scene);
+        stage.show();
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
